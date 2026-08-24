755923deb307aa1ac446f0f298ad3774d98c09d3
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-08-24 09:11:33 -0300

    feat: resolvendo o calculo de mdc usando algoritmo de euclides

--- gcd/gcd.go
+++ gcd/gcd.go
@@ -1,5 +1,34 @@
-package gcd
+package main
 
+import "fmt"
+
+// MDC/GCD significa máximo divisor comum.
+// Dados dois números, o MDC calcula o maior número pelo qual ambos os números podem ser divididos sem deixar resto.
 func GCD(a, b int) int {
-	return 0
+	// Usarei o algoritmo Euclidiano para solucionar esse problema, reduzindo-o a um problema menor.
+	// Usarei também a função recursiva para a solução.
+	// O algoritmo Euclidiano fala em: Dado um MDC(A,B), onde A != 0 e B != 0 => dividi-se A/B, resultando em um novo MDC(B,R), onde R = resto da divisão anterior,
+	// reduzindo o MDC até que um dos inteiros seja 0.
+	// As 3 Propriedades do Algoritmo são:
+		// - MDC(A,0) = A
+		// - MDC(0,B) = B
+		// - Se A = B⋅Q + R e B≠0, então MDC(A,B) = MDC(B,R) sendo Q um inteiro, e R um inteiro entre 0 e B-1 
+	fmt.Printf("MDC(%v, %v)\n", a, b)
+
+	if a == 0 && b != 0{return b}
+	if b == 0 && a != 0{return a}
+
+	r := a % b
+
+	if r == 0 {return b} else {
+		return GCD(b,r)
+	}
+
+
+}
+
+
+func main(){
+	a, b := 270,192
+	fmt.Printf("O máximo divisor comum de %v e %v é %v\n",a , b, GCD(a, b))
 }
